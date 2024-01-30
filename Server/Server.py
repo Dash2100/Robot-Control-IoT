c6f5de1f9705ea1577a7db183c138a14a1c8961b
Tony2100 <87257550+Dash2100@users.noreply.github.com>
2024-01-30 15:17:56 +0800

    b

--- Server/Server.py
+++ Server/Server.py
@@ -1,10 +1,32 @@
 from flask import Flask, Response, render_template
 import pyaudio
+import cv2
 
 app = Flask(__name__)
 
 p = pyaudio.PyAudio()
 mic = 0
+
+def generate_frames():
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            break
+        else:
+            # frame = imutils.resize(frame, width=600)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                continue
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def genHeader(sampleRate, bitsPerSample, channels):
     datasize = 2000*10**6  # Adjust estimated data size
